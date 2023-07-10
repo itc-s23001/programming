7c7e8ca0430a06ce16536c1818bea7ea22487bb6
Ahagon syoutarou <s23001@std.it-college.ac.jp>
2023-07-10 05:08:51 +0000

    rist

---
+++ chap02/Q2_7_2.py
@@ -0,0 +1,9 @@
+my_list = ["orange", "apple", "grape", "banana"]
+
+
+ascending_order = sorted(my_list)
+print("昇順:", ascending_order)
+
+
+descending_order = sorted(my_list, reverse=True)
+print("降順:", descending_order)
